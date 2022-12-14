c547cbe8d353f73e19b2aedcdba97d88c5d64c78
Sreeja <pulisaisreeja28@gmail.com>
2022-12-14 08:35:47 +0530

    First project

---
+++ src/main/java/com/FirstProj/FirstProj/App.java
@@ -0,0 +1,13 @@
+package com.FirstProj.FirstProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
